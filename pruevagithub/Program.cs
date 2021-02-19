e1f3ce9fa01c45c23e7cca7243052b7c7979845b
mardoqueocampos2019@gmail.com <mardoqueocampos2019@gmail.com>
2021-02-19 00:16:50 -0600

    prueba num2

--- pruevagithub/Program.cs
+++ pruevagithub/Program.cs
@@ -8,6 +8,9 @@ namespace pruevagithub
         {
             Console.WriteLine("pruevas de github");
             Console.WriteLine("mas mas");
+            Console.WriteLine("prueva dos");
+            Console.WriteLine("mas mas");
+
         }
     }
 }
